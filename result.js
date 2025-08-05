eca38eb416bad7b54c98fec1c30e4e14c327ba3c
Surya <suryakumar2505200@gmail.com>
2025-08-05 21:40:29 +0530

    Add files via upload

---
+++ result.js
@@ -0,0 +1,4 @@
+  if (!sessionStorage.getItem("refreshed")) {
+    sessionStorage.setItem("refreshed", "true");
+    location.reload();
+}//this is the code i get in chatgpt that is use to auto refresh but that is waste 
